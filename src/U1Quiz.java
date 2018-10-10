34496e34bdcd62c1f325f195a31ba832790d760b
Isabelle <hammeri@theellisschool.org>
2018-10-10 09:26:09 -0400

    oh well...

--- src/U1Quiz.java
+++ src/U1Quiz.java
@@ -8,32 +8,28 @@ public class U1Quiz {
 		int computerWins = 0;
 
 		int userWins = 0;
+		
+		int dieRoll = (int)(Math.random() * 6) + 1;
 
-		int dieRoll = (int)(Math.random()) * 6 + 1;
-
-		int userRoll = (int)(Math.random()) * 6 + 1;
+		int userRoll = (int)(Math.random() * 6) + 1;
 		
 		String input;
 		
-		input = JOptionPane.showInputDialog("Get ready to play!");
-	while (userWins != 10) {
-		do {
-			input = JOptionPane.showInputDialog("you lose");
-			userRoll = Integer.parseInt(input);
-		} while (userRoll < dieRoll);
+
+	while (dieRoll > userRoll) {
 		computerWins ++;
-		
-		do {
-			input = JOptionPane.showInputDialog(null, "you win");
-			userRoll = Integer.parseInt(input);
-		} while (userRoll > dieRoll);
-		userWins ++;
-		
-		do {
-			input = JOptionPane.showInputDialog(null, "it's a tie");
-			userRoll = Integer.parseInt(input);
-		} while (userRoll == dieRoll);	
+		System.out.println("1 point for the computer");
 	}
+	
+	while (dieRoll < userRoll) {
+		userWins ++;
+		System.out.println("1 point for you");
+	}
+	
+	while (dieRoll == userRoll) {
+		System.out.println("tie");
+	}
+		
 
 
 	}
